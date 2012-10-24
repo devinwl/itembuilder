1676dd08bc2cce13d55b619964fd268a527c568e
Devin Lumley <keiroshin@gmail.com>
2012-10-23 23:07:21 -0700

    Initial commit

---
+++ resources/js/itembuilder.js
@@ -0,0 +1,48 @@
+$(function() {
+
+	var $starting 	= $("#starting"),
+		$early		= $("#early"),
+		$core		= $("#core"),
+		$luxury		= $("luxury");
+
+	$.getJSON("resources/items.json", function(json) {
+		$.each(json.items, function(i, item) {
+			if(item.itemVisible == "1")
+				$("<li>").attr("name", item.itemNameBasic).attr("cost", item.itemCost).addClass("item").html($("<img/>").attr("src", "resources/images/items/"+item.itemNameBasic+".png")).appendTo("#items > ul");
+		});
+
+		$(".item").draggable({
+			revert: "invalid",
+			helper: "clone",
+			cursor: "move",
+			connectWith: ".item_container"
+		});
+	});
+
+	$(".item_container").sortable({
+		connectWith: ".item_container",
+		items: ".dropped",
+		tolerance: "pointer",
+		receive: function(event, ui) {
+			var dest = $(this).attr("id");
+			$(ui.item).appendTo("#"+dest+" ul");
+		}
+	}).droppable({
+		accept: ".item",
+		drop: function(event, ui) {
+			var name = $(ui.draggable).attr("name");
+			var cost = $(ui.draggable).attr("cost");
+			var contents = $("<li>").attr("name", name).attr("cost", cost).addClass("dropped").html($("<img/>").attr("src", "resources/images/items/"+name+".png"));
+			var dest = $(this).attr("id");
+			contents.appendTo("#" + dest + " ul");
+		}
+	});
+
+	$("#submit_build").click( function() {
+		console.log("Generate the build!");
+		$("#starting > ul > li").each( function(i, item) {
+			console.log(item);
+		});
+		return false;
+	});
+});
